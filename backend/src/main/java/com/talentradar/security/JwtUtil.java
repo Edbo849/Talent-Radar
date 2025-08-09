c5f6864800ce49c1ca9d1696b66a72b1d364b1ab
Edbo849 <edborthwick84@gmail.com>
2025-08-09 17:59:39 +0100

    Added placeholder security file

---
+++ backend/src/main/java/com/talentradar/security/JwtUtil.java
@@ -0,0 +1,14 @@
+package com.talentradar.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    public String getUsernameFromToken(String token) {
+        // Placeholder
+
+        return "placeholder-username";
+    }
+
+}
